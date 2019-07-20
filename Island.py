98382ad5864ce36bdcca00b912b1d364e08625bc
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2019-07-20 21:36:01 +0530

    a little optimization.
    instead of using bubble sort, this (code) uses Tim sort.

--- Island.py
+++ Island.py
@@ -1,54 +1,45 @@
-
 n1 = int(input())
 st = []
 cou1 = 0
-while cou1 != n:
+while cou1 != n1:
     st1 = input()
     st = st + [st1]
     cou1 = cou1 + 1
 
 war = input()
 
-final_array = []
-
+final_arr = []
 
 cou2 = 0
-l = []
 while cou2 != n1:
     li = st[cou2].split(" ")
 
-    final = [li[0]+" "+li[1]]+[li[2]+" "+li[3]]+[li[0]+" "+li[3]]+[li[2]+" "+li[1]]
-    #print(final)
-
+    final = [li[0] + " " + li[1]] + [li[2] + " " + li[3]] + [li[0] + " " + li[3]] + [li[2] + " " + li[1]]
 
     f = war.split(" ")
-
     cou = 0
     stor = []
+    count = 0
     while count != 4:
         v = final[cou]
         str1 = v.split(" ")
-        val = abs(int(f[0])-int(str1[0]))+abs(int(f[1])-int(str1[1]))
+        val = abs(int(f[0]) - int(str1[0])) + abs(int(f[1]) - int(str1[1]))
         stor = stor + [val]
 
         count = count + 1
-    # print(min(stor))
-    # final_array[count2][0] = min(stor)
-    # final_array[count2][1] = count2
-    final_arr = final_arr + [[min(stor),cou2+1]]
+
+    final_arr.append((cou2, min(stor)))
     cou2 = cou2 + 1
 
-for i in range(n1):
 
-    for j in range(0, n1 - i - 1):
+def sortingfun(y):
+    return y[1]
 
-        if final_arr[j][0] > final_arr[j + 1][0]:
-            final_arr[j][0], final_arr[j + 1][0] = final_arr[j + 1][0], final_arr[j][0]
-            final_arr[j][1], final_arr[j + 1][1] = final_array[j + 1][1], final_arr[j][1]
 
-for i in range(n1):
-    print(final_arr[i][1])
+final_arr.sort(key=sortingfun)
 
+for i in final_arr:
+    print(i[0] + 1)
 
 # 3
 # 1 1 0 0
